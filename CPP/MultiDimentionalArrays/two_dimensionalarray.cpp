92881d426734ddc65fa0b89e4a7830692a579daf
mahirx <60250437+mahirx@users.noreply.github.com>
2021-07-08 20:26:24 +0600

    created

---
+++ CPP/MultiDimentionalArrays/two_dimensionalarray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+	int x[2][3]={
+		{1,2,3}, // 1st row
+		{5,6,7}  // 2nd row
+	};
+
+
+
+		int y[2][3] = {{2, 3, 4}, {8, 9, 10}};
+
+
+
+		cout << x<< endl;
+		printf("%p\n", x);
+	
+	return 0;
+}
