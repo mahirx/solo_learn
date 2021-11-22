591442fdfa6d68b657231e3a4b2a9a711efa3438
mahirx <60250437+mahirx@users.noreply.github.com>
2021-11-22 12:19:11 +0600

    created__

---
+++ CPP/Fuctions/function_parameters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int timesTwo(int x) {
+   return x*2;
+}
+
+int main(int argc, char const *argv[])
+{
+	cout << timesTwo(2) << endl;
+
+	cout << timesTwo(3) << endl;
+
+	cout << timesTwo(5) << endl;
+	
+	return 0;
+}
