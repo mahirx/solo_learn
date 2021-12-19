03e0d365df6ad88a3ac866ea0190ed08bf43f40e
mahirx <60250437+mahirx@users.noreply.github.com>
2021-12-20 00:53:48 +0600

    created

---
+++ CPP/Fuctions/multiple_parameters.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int addNumbers(int x, int y) {
+    int result = x + y;
+    return result;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    printf("%d\n", addNumbers(7, 11));
+    return 0;
+}
