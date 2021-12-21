96a0afd588ada7769e03650bcea183d37da0d61f
mahirx <60250437+mahirx@users.noreply.github.com>
2021-12-21 23:33:40 +0600

    Update trunlyRandomNumbers.cpp

--- CPP/Fuctions/trunlyRandomNumbers.cpp
+++ CPP/Fuctions/trunlyRandomNumbers.cpp
@@ -20,3 +20,8 @@ int main(int argc, char const *argv[])
 	}
 	return 0;
 }
+
+
+// A solution to generate truly random numbers, is to use the current time as a seed value for the srand() function.
+// This example makes use of the time() function to get the number of seconds on your system time, and randomly seed the rand() function (we need to include the <ctime> header for it):
+// time(0) will return the current second count, prompting the srand() function to set a different seed for the rand() function each time the program runs.
