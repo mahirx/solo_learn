2d606f67a141d66913d842571658b0dca23a109e
mahirx <60250437+mahirx@users.noreply.github.com>
2022-01-29 01:45:17 +0600

    updatesd

---
+++ CPP/Fuctions/recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int factorial(int n) {
+  if (n==1) {
+    return 1;
+  }
+  else {
+    return n * factorial(n-1);
+  }
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+	cout << factorial(19) << endl;
+	return 0;
+}
