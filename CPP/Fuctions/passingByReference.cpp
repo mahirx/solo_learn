fdb43528ed859ea09af732f06c973657806372db
mahirx <60250437+mahirx@users.noreply.github.com>
2022-03-07 19:36:50 +0600

    created

---
+++ CPP/Fuctions/passingByReference.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void myFunc(int *x){
+	*x = 100;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int var = 32;
+	myFunc(&var);
+	cout << var << endl;
+	printf("%p", &var);
+	return 0;
+}
